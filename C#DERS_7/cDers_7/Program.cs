3975ff1677819229ec16c542b8808fc78941ee40
ozturkzehra <141491478+ozturkzehra@users.noreply.github.com>
2026-04-19 13:03:28 +0300

    ders7 eklendi ders1 rename

---
+++ C#DERS_7/cDers_7/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cDers_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region foreach dongusu 
+            //foreach (1;2;3;4)
+            //1:Değişkenin türü 
+            //2:Değişkenin adı
+            //3:In
+            //4:Liste, dizi, koleksiyon
+
+            //string[] cities = { "milano", "londra", "ankara", "izmir" };
+            //foreach (string x in cities)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //int[] nums = { 1, 2, 3, 4, 5, 6, 6 };
+            //foreach (int nums in nums)
+            //{
+            //    Console.WriteLine(nums);
+            //}
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,3,4,6,7,8,9
+            //};
+
+            //foreach(int slay in numbers)
+            //{
+            //    Console.WriteLine(slay);
+            //}
+
+            //string word = "Merhaba";
+            //foreach(char c in word)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+
+           
+
+
+
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
